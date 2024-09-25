70013319953824dcc9faa3b26a9d0fb493959ffd
emmanuelmejia1516 <a.23308051281197@cbtis128.edu.mx>
2024-09-24 19:51:13 -0600

    Add files via upload

---
+++ multiplicacion.py
@@ -0,0 +1,13 @@
+# Solicitar tres números al usuario
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+num3 = float(input("Ingresa el tercer número: "))
+# Realizar la multiplicación
+resultado = num1 * num2 * num3
+# Mostrar el resultado
+print(f"La multiplicación de los tres números es: {resultado}")
+
+
+print("")
+print("Mejia Suarez Emmanuel Alexander:mi practica es un codigo donde solicite tres numeros y se realice la multiplicacion de ellos")
+print("")
